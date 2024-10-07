b665798033ae0a7e9059e2fdc0b31a273ab49454
clases <fgonaci085@g.educaand.es>
2024-10-07 10:02:45 +0200

    Ventanas de alerta implementadas

--- src/main/java/com/example/ejemploalerts/HelloController.java
+++ src/main/java/com/example/ejemploalerts/HelloController.java
@@ -1,14 +1,74 @@
 package com.example.ejemploalerts;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+
+import java.util.Optional;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label lblResultadoModal;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void onBtnModalSiNo(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmación");
+        alert.setHeaderText("Ha ocurrido un error");
+        alert.setContentText("¿ Desea intentar volver a conectarse a la red ?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            lblResultadoModal.setText("Resultado del Alert Modal - Aceptar");
+        } else {
+            lblResultadoModal.setText("Resultado del Alert Modal - Cancelar");
+        }
+
+    }
+
+
+    @FXML
+    public void onBtnAdvertencia(ActionEvent actionEvent) {
+        // Crear una alerta de tipo error
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Advertencia");
+        alert.setHeaderText("Advertencia");
+        alert.setContentText("No   se pudo conectar al servidor.");
+
+        // Mostrar la alerta
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void onBtnError(ActionEvent actionEvent) {
+        // Crear una alerta de tipo error
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Ha ocurrido un error");
+        alert.setContentText("No   se pudo conectar al servidor.");
+
+                // Mostrar la alerta
+                alert.showAndWait();
+    }
+
+    @FXML
+    public void onBtnInformacion(ActionEvent actionEvent) {
+        // Crear una alerta de tipo error
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Información");
+        alert.setHeaderText("Ha ocurrido un error");
+        alert.setContentText("No   se pudo conectar al servidor.");
+
+        // Mostrar la alerta
+        alert.showAndWait();
     }
 }
